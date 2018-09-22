c54a40da020d0ff2c5908a7d9c68098ec18a813a
skpulipaka26 <31779125+skpulipaka26@users.noreply.github.com>
2018-09-21 23:23:33 -0700

    Implemented the microsoft face api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'face-match';
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': '42280d5b97f44154bb2fb839908eef9f'
+      })
+    };
+    this.http
+      .post('https://westus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false',
+        file, httpOptions).subscribe(v => console.log(v));
+  }
+
 }
